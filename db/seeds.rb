74f77c5ec62330676dd5cddb67f62b3783dcf573
Timur Vafin <timur.vafin@flatsoft.com>
2010-07-15 17:41:22 +0400

    Remove test data from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,5 @@
-user1 = User.create(:full_name => 'Timur Vafin', :email => 'me@timurv.ru', :password => '123456')
-user2 = User.create(:full_name => 'Test User', :email => 'me2@timurv.ru', :password => '123456')
+User.create(:full_name => 'Timur Vafin', :email => 'me@timurv.ru', :password => '123456')
 
 %W(101 102 103 104 200 201 202 202A 203).each do |room_name|
-  room = Room.create(:name => room_name)
-
-  # test data
-  1..20.times do |n|
-    Unit.create(
-      :unit_type => APP_CONFIG['unit_types'].sample,
-      :name => "Mac mini #{n}",
-      :description => '2GB',
-      :room_id => room.id,
-      :user_id => user1.id,
-      :on_depot => false
-    )
-  end
+  Room.create(:name => room_name)
 end
