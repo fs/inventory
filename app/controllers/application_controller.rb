e35d60bed188752d006d301acb6a94e1d8256b4c
Timur Vafin <timur.vafin@flatsoft.com>
2010-07-19 19:21:56 +0400

    9 unit per page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def search(scope)
+  def search(scope, per_page = 10)
     params[:search] ||= {}
 
-    return scope.search_for(params[:search][:query]).paginate(:per_page => 10, :page => params[:page]),
+    return scope.search_for(params[:search][:query]).paginate(:per_page => per_page, :page => params[:page]),
            ::OpenStruct.new(:query => params[:search][:query])
   end
 end
